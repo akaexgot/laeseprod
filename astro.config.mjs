e31666b9068cfa03bb9889f7addbb5e41f6d94a3
akaexgot <joaquinrivtir@gmail.com>
2026-04-09 20:35:14 +0200

    fix: make site domain agnostic for production testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://videomarketingsevilla.es',
+  // site: 'https://videomarketingsevilla.es',
   output: 'server',
 
   vite: {
